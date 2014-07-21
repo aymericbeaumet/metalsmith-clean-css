8d020373763dfca138a901e1fdecd49530e1fcc5
Aymeric Beaumet <aymeric@beaumet.me>
2014-07-21 03:36:18 +0200

    Working plugin

--- README.md
+++ README.md
@@ -1,4 +1,63 @@
-metalsmith-css-clean
-====================
+# metalsmith-clean-css [![Build Status](https://travis-ci.org/aymericbeaumet/metalsmith-clean-css.svg?branch=master)](https://travis-ci.org/aymericbeaumet/metalsmith-clean-css) [![NPM version](https://badge.fury.io/js/metalsmith-clean-css.svg)](http://badge.fury.io/js/metalsmith-clean-css)
 
-A Metalsmith plugin to concatenate and minify CSS files
+A Metalsmith plugin to minify CSS files using
+[clean-css](https://github.com/GoalSmashers/clean-css).
+
+## Installation
+
+```javascript
+$ npm install metalsmith-clean-css
+```
+
+## Usage
+
+### CLI
+
+```javascript
+{
+  "plugins": {
+    "metalsmith-clean-css": {
+      "pattern": "src/**/*.css"
+    }
+  }
+}
+```
+
+### JavaScript
+
+```javascript
+var MetalSmith = require('metalsmith');
+var cleanCSS = require('metalsmith-clean-css');
+
+Metalsmith(__dirname)
+  .use(cleanCSS({
+    files: "src/**/*.css",
+    cleanCSS: {
+      noRebase: true
+    }
+  }));
+```
+
+#### files
+Type: `String`
+Default: `**/*.css`
+
+This define which files pattern are concerned by the minification. It is
+directly passed to [minimatch](https://github.com/isaacs/minimatch).
+
+#### cleanCSS
+Type: `Object`
+Default: `{}`
+
+Allow you to directly manipulate the [CleanCSS
+API](https://github.com/GoalSmashers/clean-css#how-to-use-clean-css-programmatically).
+The configuration object will be passed as is.
+
+## Changelog
+
+* 0.0.1
+  * Working plugin
+
+## License
+
+MIT © [Aymeric Beaumet](http://beaumet.me)
---
+++ test/metalsmith-clean-css.js
@@ -0,0 +1,64 @@
+var chai = require('chai');
+
+var Metalsmith = require('metalsmith');
+var cleanCSS = require('../lib');
+
+var expect = chai.expect;
+
+describe('metalsmith-clean-css', function() {
+
+
+  it('should not match any file if an empty string is given', function(done) {
+    var files = {
+      'main.css': { content: '  * { display: none; }  ' }
+    };
+
+    cleanCSS({ files: '' })(files, null, function() {
+      expect(files['main.css'].content).to.equal('  * { display: none; }  ');
+      done();
+    });
+  });
+
+
+  it('should match any CSS file with the default pattern', function(done) {
+    var files = {
+      'main.css':           { content: '  * { display: none; }  ' }
+    , 'long/path/main.css': { content: '  * { display: none; }  ' }
+    };
+
+    cleanCSS()(files, null, function() {
+      Object.keys(files).forEach(function(filename) {
+        expect(files[filename].content).to.equal('*{display:none}');
+      });
+      done();
+    });
+  });
+
+
+  it('should only match the desired CSS files if a pattern is given', function(done) {
+    var files = {
+      'main.css':           { content: '  * { display: none; }  ' },
+      'long/path/main.css': { content: '  * { display: none; }  ' }
+    };
+
+    cleanCSS({ files: '*.css' })(files, null, function() {
+      expect(files['main.css'].content).to.equal('*{display:none}');
+      expect(files['long/path/main.css'].content).to.equal('  * { display: none; }  ');
+      done();
+    });
+  });
+
+
+  it('should correctly pass options to clean-css', function(done) {
+    var files = {
+      'main.css': { content: '/*! special comment */' }
+    };
+
+    cleanCSS({ cleanCSS: { keepSpecialComments: 0 } })(files, null, function() {
+      expect(files['main.css'].content).to.be.empty;
+      done();
+    });
+  });
+
+
+});
